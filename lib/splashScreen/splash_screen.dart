87cd56a2a6143eb0c5ca1f2292e169cd4cc9d20c
JairBarco <jair_barco@outlook.com>
2022-08-18 13:53:02 -0500

    Initial commit

---
+++ lib/splashScreen/splash_screen.dart
@@ -0,0 +1,61 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+import '../global/global.dart';
+import '../mainScreens/main_screen.dart';
+
+class MySplashScreen extends StatefulWidget {
+  const MySplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MySplashScreen> createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+
+  startTimer(){
+    Timer(const Duration(seconds:3),() async {
+      if(fAuth.currentUser !=null){
+        currentFirebaseUser = fAuth.currentUser;
+        Navigator.push(context, MaterialPageRoute(builder: (c)=> MainScreen()));
+      } else {
+        //send user to main screen
+        //Navigator.push(context, MaterialPageRoute(builder: (c)=> LoginScreen()));
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    startTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        color: Colors.black,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("img/logo.png"),
+              const SizedBox(height: 10,),
+              const Text(
+                "Ship Driver",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
+                ),
+              )
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
