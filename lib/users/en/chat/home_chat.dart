7bc04a4695249b21d6b01ab5f4c7b4aa0f4c6254
JairBarco <jair_barco@outlook.com>
2022-09-15 16:49:27 -0500

    Chat added

--- lib/users/en/chat/home_chat.dart
+++ lib/users/en/chat/home_chat.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
+
+import 'friends_chat.dart';
 
 class HomeChat extends StatefulWidget {
-  const HomeChat({Key? key}) : super(key: key);
-
   @override
   State<HomeChat> createState() => _HomeChatState();
 }
 
 class _HomeChatState extends State<HomeChat> {
+  final _usernameController = TextEditingController();
+  String? _usernameError;
+  bool _loading = false;
+
+  Future<void> _onGoPressed() async {
+
+    final username = _usernameController.text;
+    if(username.isNotEmpty){
+      final client = StreamChat.of(context).client;
+      setState(() {
+        _loading = true;
+      });
+      await client.setUser(
+        User(id: username, extraData: {
+          'image': DataUtils.getUserImage(username),
+        }),
+        client.devToken(username),
+      );
+      setState(() {
+        _loading = false;
+      });
+
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: (_) => FriendsChat(),
+      ),);
+    } else {
+      setState(() {
+        _usernameError = 'Username is not valid';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,19 +48,20 @@ class _HomeChatState extends State<HomeChat> {
         title: Text('Chat'),
       ),
       body: Center(
-        child: Card(
+        child: _loading ? CircularProgressIndicator(): Card(
           elevation: 11,
-          margin: const EdgeInsets.all(8.0),
+          margin: const EdgeInsets.all(15.0),
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(20.0),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
                 Text('Welcome to support chat'),
                 TextField(
-                  controller: null,
+                  controller: _usernameController,
                   decoration: InputDecoration(
-                    hintText: 'Username'
+                    hintText: 'Username',
+                    errorText: _usernameError,
                   ),
                 ),
                 ElevatedButton(
