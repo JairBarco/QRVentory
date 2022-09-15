f4ed44b8aee1878eac5813060eb9fdf279b76eac
abril bejarano <abrlbjrn@gmail.com>
2022-09-15 00:12:11 -0500

    Stream chat part was created.
    All the necessary files and folders were created.
    Also config.dart, but without the DB part.

---
+++ lib/users/en/mainScreens/config.dart
@@ -0,0 +1,11 @@
+class Config {
+  static final String apiKey = '';
+
+  static final String tokenEmily =
+      '';
+  static final String idEmily = '';
+
+  static final String tokenPeter =
+      '';
+  static final String idPeter = '';
+}
--- lib/users/en/mainScreens/help_screen.dart
+++ lib/users/en/mainScreens/help_screen.dart
@@ -1,97 +1,105 @@
+import 'package:chat_stream_example/api/stream_api.dart';
+import 'package:chat_stream_example/config.dart';
+import 'package:chat_stream_example/page/channel_list_page.dart';
 import 'package:flutter/material.dart';
-import 'package:dialog_flowtter/dialog_flowtter.dart';
-import 'package:users_app/users/en/mainScreens/messages.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
-class HelpScreen extends StatefulWidget {
-  const HelpScreen({Key? key}) : super(key: key);
+Future main() async {
+  final client = Client(Config.apiKey, logLevel: Level.SEVERE);
 
-  @override
-  State<HelpScreen> createState() => _HelpScreenState();
+  await StreamApi.initUser(
+    client,
+    username: 'Emily',
+    urlImage:
+    'https://cdn1.iconfinder.com/data/icons/user-pictures/100/female1-512.png',
+    id: Config.idEmily,
+    token: Config.tokenEmily,
+  );
+
+//  await StreamApi.initUser(
+//    client,
+//    username: 'Peter',
+//    urlImage:
+//        'https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg',
+//    id: Config.idPeter,
+//    token: Config.tokenPeter,
+//  );
+
+//  final channel = await StreamApi.createChannel(
+//    client,
+//    type: 'messaging',
+//    id: 'sample2',
+//    name: 'Family',
+//    image:
+//        'https://image.freepik.com/fotos-kostenlos/glueckliche-familie-in-einer-reihe-liegen_1098-1101.jpg',
+//    idMembers: [Config.idEmily, Config.idPeter],
+//  );
+
+  final channel = await StreamApi.watchChannel(
+    client,
+    type: 'messaging',
+    id: 'sample',
+  );
+
+  runApp(MyApp(client: client, channel: channel));
 }
 
-class _HelpScreenState extends State<HelpScreen> {
+class MyApp extends StatelessWidget {
+  final Client client;
+  final Channel channel;
+
+  const MyApp({Key key, this.client, this.channel}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ChatBot',
-      theme: ThemeData(brightness: Brightness.dark),
-      home: Home(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: MyHomePage(title: 'Stream Chat', client: client, channel: channel),
     );
   }
 }
 
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  final Client client;
+  final Channel channel;
+
+  MyHomePage({Key key, this.title, this.client, this.channel})
+      : super(key: key);
+
+  final String title;
 
   @override
-  _HomeState createState() => _HomeState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _HomeState extends State<Home> {
-  late DialogFlowtter dialogFlowtter;
-  final TextEditingController _controller = TextEditingController();
-
-  List<Map<String, dynamic>> messages = [];
-
-  @override
-  void initState() {
-    DialogFlowtter.fromFile().then((instance) => dialogFlowtter = instance);
-    super.initState();
-  }
-
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Drivers Bot'),
+        title: Text(widget.title),
       ),
-      body: Container(
-        child: Column(
-          children: [
-            Expanded(child: MessagesScreen(messages: messages)),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 14, vertical: 8),
-              color: Colors.deepPurple,
-              child: Row(
-                children: [
-                  Expanded(
-                      child: TextField(
-                        controller: _controller,
-                        style: TextStyle(color: Colors.white),
-                      )),
-                  IconButton(
-                      onPressed: () {
-                        sendMessage(_controller.text);
-                        _controller.clear();
-                      },
-                      icon: Icon(Icons.send))
-                ],
-              ),
-            )
-          ],
+      body: StreamChat(
+        streamChatThemeData: StreamChatThemeData(
+          otherMessageTheme: MessageTheme(
+            messageText: TextStyle(
+                fontWeight: FontWeight.bold, color: Colors.black, fontSize: 20),
+          ),
+          ownMessageTheme: MessageTheme(
+            messageText: TextStyle(
+                fontWeight: FontWeight.bold, color: Colors.black, fontSize: 20),
+            avatarTheme: AvatarTheme(
+              constraints: BoxConstraints(maxWidth: 80, maxHeight: 80),
+              borderRadius: BorderRadius.circular(120),
+            ),
+          ),
         ),
+        child: ChannelListPage(),
+        client: widget.client,
       ),
     );
-  }
-
-  sendMessage(String text) async {
-    if (text.isEmpty) {
-      print('Message is empty');
-    } else {
-      setState(() {
-        addMessage(Message(text: DialogText(text: [text])), true);
-      });
-
-      DetectIntentResponse response = await dialogFlowtter.detectIntent(
-          queryInput: QueryInput(text: TextInput(text: text)));
-      if (response.message == null) return;
-      setState(() {
-        addMessage(response.message!);
-      });
-    }
-  }
-
-  addMessage(Message message, [bool isUserMessage = false]) {
-    messages.add({'message': message, 'isUserMessage': isUserMessage});
   }
 }
